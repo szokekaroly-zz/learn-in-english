feb2a80d46ce7d6653596f1d639cf0e2c2c6d2e8
szoke.karcsi@gmail.com <szoke.karcsi@gmail.com@fcc020ea-5fe2-18fb-c80b-64afecbc5458>
2014-08-05 19:01:58 +0000

    Menü miért inaktív?

--- View/Classic/LearnMenu.xaml.cs
+++ View/Classic/LearnMenu.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Learn.View.Classic
 {
@@ -32,12 +22,17 @@ namespace Learn.View.Classic
 
         private void Open_Click(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("open");
+            throw new NotImplementedException();
         }
 
         private void New_Click(object sender, ExecutedRoutedEventArgs e)
         {
+            throw new NotImplementedException();
+        }
 
+        private void Save_Click(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
     }
