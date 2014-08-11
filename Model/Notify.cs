bb98efe6e733ccd2371bc07e528e6112f223858d
szoke.karcsi@gmail.com <szoke.karcsi@gmail.com@fcc020ea-5fe2-18fb-c80b-64afecbc5458>
2014-08-11 21:05:49 +0000

    Menük és Commandok összekapcsolása, eseménykezelők.

--- Model/Notify.cs
+++ Model/Notify.cs
@@ -10,11 +10,14 @@ namespace Learn.Model
         [field:NonSerialized]
         public event PropertyChangedEventHandler PropertyChanged;
 
+        protected bool IsChanged { get; private set; }
+
         protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
         {
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                IsChanged = true;
             }
         }
     }
