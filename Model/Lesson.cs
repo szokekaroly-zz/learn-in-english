baa493ee8fa2fc196c0bd6cdd1731b29dc18987e
szoke.karcsi@gmail.com <szoke.karcsi@gmail.com@fcc020ea-5fe2-18fb-c80b-64afecbc5458>
2014-08-12 19:00:16 +0000

    szópár szerkesztésénél miért nem látszik a gomb felirata, mikor a command hozzá van rendelve?

--- Model/Lesson.cs
+++ Model/Lesson.cs
@@ -5,5 +5,12 @@ namespace Learn.Model
     [Serializable]
     public class Lesson:CustomItems<Word>
     {
+        public Word CreateWord()
+        {
+            Word word = new Word();
+            Items.Add(word);
+            NotifyPropertyChanged();
+            return word;
+        }
     }
 }
